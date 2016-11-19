eb0ee70a5c9827822e3fc4470a635df8df8e1409
Jim Cummins <jimthedev@gmail.com>
2016-11-19 07:02:12 -0600

    Use es6 fat arrow for routes

--- src/index.js
+++ src/index.js
@@ -4,35 +4,35 @@ var express = require('express');
 // Create a new express app, called app.
 var app = express();
 
-// Create our first route
-app.get('/', function(req, res) {
+// Create our first routem
+app.get('/', (req, res) => {
   res.send('Hello world');
 });
 
 // Create our first route
-app.get('/robot', function(req, res) {
+app.get('/robot', (req, res) => {
   res.json({
     name: 'R2D2',
     lastUpdated: new Date()
   });
 });
 
-app.post('/robot', function (req, res) {
+app.post('/robot', (req, res) => {
   res.send('Got a POST request to create a robot')
 })
 
-app.put('/robot', function (req, res) {
+app.put('/robot', (req, res) => {
   res.send('Got a PUT request to modify a robot ')
 })
 
-app.delete('/robot/:robotId', function (req, res) {
+app.delete('/robot/:robotId', (req, res) => {
   res.send('Got a DELETE request for robot ' + req.params.robotId);
 })
 
-app.delete('/robot/:robotId/part/:partId', function (req, res) {
+app.delete('/robot/:robotId/part/:partId', (req, res) => {
   res.send('Got a DELETE request to delete the part (' + req.params.partId + ') on robot ' + req.params.robotId);
 })
 
-app.listen(8000, function () {
+app.listen(8000, () => {
   console.log('Example app listening on port 8000!')
 })
